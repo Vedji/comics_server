94a68bf3a34d400a60625e7cf78d0e08e8f57e19
Ponkratov Nickolay <45602069+Vedji@users.noreply.github.com>
2024-02-21 19:22:14 +0300

    Add files via upload
    Add function filter genres

--- static/js_scripts/manga_catalog.js
+++ static/js_scripts/manga_catalog.js
@@ -1,17 +1,23 @@
-var SCROLL_ID = 0;
-var LIMIT_SCROLL = 30;
-var table_name_ = "manga";
+let SCROLL_ID = 0;
+const LIMIT_SCROLL = 30;
+const table_name_ = "manga";
+let template_form = "________________";
+const manga_genres = [
+        "Романтика", "Драма", "Комедия", "Фэнтези", "Приключения", "Повседневность", "Киберпанк", "Психологическое",
+        "Космос", "Магия", "Фантастика", "Пост-апокалипсис", "Боевик", "Меха", "Сверхъестественное", "Детектив"
+    ];
+
+
 function addItemsToBD(scroll_num){
-fetch("/api/bd/get_list", {
-    method: 'POST',
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify({
-        table_name: table_name_,
-        limit: LIMIT_SCROLL,
-        offset: scroll_num * LIMIT_SCROLL
-    })}
-).then(response => response.json())
-.then(data => {
+    fetch("/api/manga/get_catalog", {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            limit: LIMIT_SCROLL,
+            offset: scroll_num * LIMIT_SCROLL,
+            template: template_form
+        })}
+    ).then(response => response.json()).then(data => {
         if (data["result"]){
             for (let i = 0; i < data["data_list"].length; i++){
                 let url_manga_read = '/catalog_manga/' + data["data_list"][i]["manga_name"] + '/';
@@ -27,16 +33,57 @@ fetch("/api/bd/get_list", {
                 document.getElementById("catalog-manga").innerHTML += item;
             }
         }
-    })
-    .catch(error => {
+    }).catch(error => {
         console.log(error);
     });
 }
-addItemsToBD(SCROLL_ID);
-SCROLL_ID += 1;
+
+
+function main() {
+    // Create buttons for input manga genres
+    let manga_genres_container = document.getElementById("filter-genre-container");
+    for(let i = 0; i < manga_genres.length; i++){
+        element_btn = "<div><input type='checkbox' id='btn_genre_" + i + "' name='genre_" + i + "' checked/>";
+        element_btn += "<label htmlFor='btn_genre_" + i + "'>" + manga_genres[i] + "</label></div>";
+        manga_genres_container.innerHTML += element_btn;
+    }
+    for(let i = 0; i < manga_genres.length; i++){
+        let btn = document.getElementById("btn_genre_" + i);
+        btn.checked = false;
+        btn.value = i;
+    }
+    addItemsToBD(SCROLL_ID);
+    SCROLL_ID += 1;
+}
+
+
 window.addEventListener('scroll', function() {
-            if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-                addItemsToBD(SCROLL_ID);
-                SCROLL_ID += 1;
-            }
-        });
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+        addItemsToBD(SCROLL_ID);
+        SCROLL_ID += 1;
+    }
+});
+
+
+let button = document.getElementById("set-filters-button");
+button.addEventListener("click", function (event){
+    event.preventDefault();
+    let temp = "";
+    for (let i = 0; i < manga_genres.length; i++){
+        let btn_ch = document.getElementById("btn_genre_" + i);
+        if (btn_ch.checked){
+            temp = temp + "1";
+        }else{
+            temp = temp + "_";
+        }
+    }
+    template_form = temp;
+    let catMan = document.getElementById("catalog-manga");
+    catMan.innerHTML = '';
+    SCROLL_ID = 0;
+    addItemsToBD(SCROLL_ID);
+    SCROLL_ID += 1;
+})
+
+
+main();
